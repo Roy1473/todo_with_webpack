ef50451e5d4615f3acc279cbb6495caa2c0e9586
Roy1473 <pgajdmga@gmail.com>
2020-11-26 15:05:24 +0900

    post todo

---
+++ src/js/modules/html-util.js
@@ -0,0 +1,48 @@
+export function escapeSpecialCharts(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+/**
+ *  HTML文字列からHTML要素を作成して返す
+ * @param {sting} html
+ */
+export function htmlToElement(html) {
+  const template = document.createElement("template");
+  template.innerHTML = html;
+  return template.content.firstElementChild;
+}
+
+/**
+ *
+ * @param {*} strings
+ * @param  {...any} values
+ * @return {element}
+ */
+export function element(strings, ...values) {
+  const htmlString = strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+      return result + escapeSpecialCharts(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
+  console.log(htmlString);
+  return htmlToElement(htmlString);
+}
+/**
+ * コンテナ要素の中身をbodyElementで上書きする
+ * @param {Element} bodyElement コンテナ要素の中身となる要素
+ * @param {Element} containerElement コンテナ要素
+ */
+export function render(bodyElement, containerElement) {
+  // containerElementの中身を空にする
+  containerElement.innerHTML = "";
+  // containerElementの直下にbodyElementを追加する
+  containerElement.appendChild(bodyElement);
+}
--- src/js/modules/sub.js
+++ src/js/modules/sub.js
@@ -1,3 +1,4 @@
+import { element } from "./html-util.js";
 export function hello(greeting) {
   return `${greeting}, テスト`;
 }
@@ -5,8 +6,16 @@ export class App {
   mount() {
     const formElement = document.querySelector("#js-form");
     const inputElement = document.querySelector("#js-form-input");
+    const containerElement = document.querySelector("#js-todo-list");
+    const todoItemCountElement = document.querySelector("#js-todo-count");
+    let todoItemCount = 0;
     formElement.addEventListener("submit", (event) => {
       event.preventDefault();
+      const todoItemElement = element`<li>${inputElement.value}</li>`;
+      containerElement.appendChild(todoItemElement);
+      todoItemCount += 1;
+      todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+      inputElement.value = "";
       console.log(`入力値の値: ${inputElement.value}`);
     });
   }
